406aca156420db3250e5a192f7086404dc4dd6e9
Umesh <sahuumesh540@gmail.com>
2024-07-22 07:33:56 +0530

    Version 1.0.5

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,15 +1,13 @@
 <x-layout>
     <x-slot:heading>
-        Create Job
+        Register
     </x-slot:heading>
 
     <form method="POST" action="/jobs">
         @csrf
         <div class="space-y-12">
             <div class="border-b border-gray-900/10 pb-12">
-                <h2 class="text-base font-semibold leading-7 text-gray-900">Profile</h2>
-                <p class="mt-1 text-sm leading-6 text-gray-600">This information will be displayed publicly so be careful
-                    what you share.</p>
+          
 
                 <div class="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
                     <x-form-field>
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\JobController;
+use App\Http\Controllers\RegisterUserController;
 use Illuminate\Support\Facades\Route;
 
 
@@ -8,3 +9,6 @@ Route::view('/', 'home');
 Route::view('/contact', 'contact');
 
 Route::resource('jobs', JobController::class);
+
+// Auth
+Route::get('/register', [RegisterUserController::class, 'create']);
